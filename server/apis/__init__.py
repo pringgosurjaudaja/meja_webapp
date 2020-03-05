6aecd84aab31440d2949cc4ec9b8d73a204941bc
bathara21 <z5123197@student.unsw.edu.au>
2020-03-05 17:17:44 +1100

    login and signup backend

--- server/apis/__init__.py
+++ server/apis/__init__.py
@@ -1,11 +1,14 @@
 from flask_restplus import Api
 from apis.menu import menu 
+from apis.auth import auth, authorizations
 # from .auth import api as auth_service
 # from .order import api as order_service
 
 api = Api(
     title= 'Meja Backend Service',
-    description= 'Backend server for Meja App'
+    description= 'Backend server for Meja App',
+    authorizations= authorizations
 )
 
 api.add_namespace(menu)
+api.add_namespace(auth)
