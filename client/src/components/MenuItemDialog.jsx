8892db281c775473e187b2d350f7bd4ed55612da
Elena Hadinata <elhadinata@192-168-1-101.tpgi.com.au>
2020-03-09 01:28:12 +1100

    minus backend

--- client/src/components/MenuItemDialog.jsx
+++ client/src/components/MenuItemDialog.jsx
@@ -1,48 +1,47 @@
 import React from 'react';
-import { Modal } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { Card } from 'react-bootstrap';
-import { Alert } from 'react-bootstrap';
+import { Modal, Button, Card, Alert } from 'react-bootstrap';
 import InputNumber from 'rc-input-number';
 import 'rc-input-number/assets/index.css';
 import 'styles/styles.css';
+import axios from 'utilities/helper';
 import example from './assets/test.jpg';
 
 export class MenuItemDialog extends React.Component {
     constructor(props) {
         super(props);
         this.handleAddCart = this.handleAddCart.bind(this);
+        this.handleQuantityChange = this.handleQuantityChange.bind(this);
         this.state = {
             addCart: false,
-            amount: '',
+            quantity: 0,
+            amount: 0,
             notes: '',
-            status: '',
-            order_id: ''
+            order_id: null
         }
     }
 
-    handleAddCart(e) { 
+    handleQuantityChange(e) {
+        this.setState({ value: e.value });
+        console.log("QUANTITY: " + e.value);
+    }
+
+    handleAddCart(e) {
         e.preventDefault();
         let obj = {};
 
         obj["menu_item_id"] = this.props.id;
-        obj["amount"] = this.state.price;
+        obj["amount"] = this.state.quantity * this.props.price;
         obj["notes"] = this.state.notes;
-        obj["status"] = false;
-        // obj["order_id"] = '';
-
-        const instance = axios.create({
-            baseURL: 'http://127.0.0.1:5000',
-            timeout: 100,
-        });
+        obj["order_id"] = 1;
         console.log(obj);
-        instance.get('/menu')
-            .then(function(response) {
+
+        axios.post('/user', { obj })
+            .then(function (response) {
                 console.log(response);
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log(error);
-            })
+            });
 
         this.setState({ addCart: true });
         console.log("added to cart");
@@ -56,23 +55,24 @@ export class MenuItemDialog extends React.Component {
                     <Modal.Header closeButton>
                     </Modal.Header>
                     <Modal.Body>
-                        <Card.Img variant="top" src={example}/>
-                        <Modal.Title>{ this.props.name }</Modal.Title>
-                        <p>{ this.props.description }</p>
-                        <p><InputNumber focusOplaceholder="Quantity" min={1} defaultValue={1}/></p>
+                        <Card.Img variant="top" src={example} />
+                        <Modal.Title>{this.props.name}</Modal.Title>
+                        <p>{this.props.description}</p>
+                        <InputNumber onChange={this.handleQuantityChange} focusOplaceholder="Quantity" min={1} defaultValue={1} />
+                        <p></p>
                         <Button onClick={this.handleAddCart} data-dismiss="modal" variant="primary">
                             Add to cart
                         </Button>
-                        {this.state.addCart && 
+                        {this.state.addCart &&
                             <Alert variant="success">
-                            Added to cart.
+                                Added to cart.
                             </Alert>
                         }
                     </Modal.Body>
                     <Modal.Footer>
 
-                    <Button onClick={this.props.onHide} variant="secondary">
-                        Close
+                        <Button onClick={this.props.onHide} variant="secondary">
+                            Close
                     </Button>
                     </Modal.Footer>
 
