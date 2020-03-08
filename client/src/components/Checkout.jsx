f637c61b752128a73925ac9597277101aa0efd03
Elena Hadinata <elhadinata@192-168-1-101.tpgi.com.au>
2020-03-08 23:40:19 +1100

    added checkout, sans backend

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import InputNumber from 'rc-input-number';
+import 'rc-input-number/assets/index.css';
 
 export class Checkout extends React.Component {
     render () {
         return (
-            
-            <h1>checkout</h1>
+            <div class="margin-center">
+                <h1>Order</h1>
+                <Card {...this.props} style={{ width: '95%' }}>
+                    <Card.Body>
+                        <Card.Text>
+                            <div align="right"><FontAwesomeIcon icon={faTrash}/></div>
+                            Menu item<br></br>
+                            <small className="text-muted">This is a custom note added by the customer</small>
+                            <InputNumber focusOplaceholder="Quantity" min={1} defaultValue={1}/>
+                            <div class="price" align="right">$16.00</div>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <br></br>
+                <p align="center"><Button>Back to menu</Button></p>
+                <p align="center"><Button>Order now</Button></p>
+            </div>
         );
     }
 }
+
+export default Checkout;
--- client/src/components/MenuItemDialog.jsx
+++ client/src/components/MenuItemDialog.jsx
@@ -2,25 +2,48 @@ import React from 'react';
 import { Modal } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
+import InputNumber from 'rc-input-number';
+import 'rc-input-number/assets/index.css';
+import 'styles/styles.css';
 import example from './assets/test.jpg';
 
 export class MenuItemDialog extends React.Component {
     constructor(props) {
         super(props);
+        this.handleAddCart = this.handleAddCart.bind(this);
+        this.state = {
+            addCart: false
+        }
     }
+
+    handleAddCart() {
+        this.setState({ addCart: true });
+        console.log("added to cart");
+        return this.props.onHide;
+    }
+
+
 
     render() {
         return (
-            <Modal {...this.props}>
+            <div>
+            <Modal dialogClassName='menu-item-dialog' {...this.props}>
                 <Modal.Header closeButton>
                 </Modal.Header>
                 <Modal.Body>
                     <Card.Img variant="top" src={example}/>
                     <Modal.Title>{ this.props.name }</Modal.Title>
                     <p>{ this.props.description }</p>
-                    <Button variant="primary">
+                    <p><InputNumber focusOplaceholder="Quantity" min={1} defaultValue={1}/></p>
+                    <Button onClick={this.handleAddCart} data-dismiss="modal" variant="primary">
                         Add to cart
                     </Button>
+                    {this.state.addCart && 
+                        <Alert variant="success">
+                           Added to cart.
+                        </Alert>
+                    }
                 </Modal.Body>
                 <Modal.Footer>
 
@@ -30,6 +53,7 @@ export class MenuItemDialog extends React.Component {
                 </Modal.Footer>
 
             </Modal>
+            </div>
         )
     }
 }
