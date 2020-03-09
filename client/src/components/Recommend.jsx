72b3295697beb61facfabe2a45e809e9ac175116
jmalem <z5141970@student.unsw.edu.au>
2020-03-09 16:56:01 +1100

    AR-66 Menu working correctly
    Menu working correctly

--- client/src/components/Recommend.jsx
+++ client/src/components/Recommend.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
-
+import { Container, Row, Col } from 'react-bootstrap';
+import { MenuItem } from 'components/MenuItem';
 export class Recommend extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuItemList: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ menuItemList: this.props.menuItemList });
+    }
+    
+
     render () {
-        return (
+        let entries = [];
+        
+        this.props.menuItemList.length > 0 && this.props.menuItemList.forEach((category) => {
             
-            <h1>rec</h1>
+            category.menu_items.length > 0 
+                && category.menu_items.forEach((item, i) => {
+                let props = {
+                    name: item.name,
+                    description: item.description,
+                    media_urls: item.media_urls,
+                    price: item.price,
+                    labels: item.labels,
+                    tags: item.tags,
+                }
+                let entry = (
+                    <Row key={i} className="layout--menu">
+                        <Col>
+                            <MenuItem className="menu-item" {...props}/>
+                        </Col>
+                    </Row>
+                );
+                entries.push(entry);
+            })
+
+        });
+
+        return (
+            <Container className="layout--padding--menu">
+                {entries}
+            </Container>
         );
     }
 }
