7a8590658e307a488e39fa939f7d502b67ce8702
Rama Pringgo <r.surjaudaja@student.unsw.edu.au>
2020-03-10 22:05:14 +1100

    fixed order

--- server/apis/order.py
+++ server/apis/order.py
@@ -6,10 +6,12 @@ from bson.objectid import ObjectId
 import json
 from marshmallow import ValidationError
 from apis.order_schema import OrderSchema, OrderItemSchema
+# from apis.menu import menu_db
 
-order = Namespace('order', description='Order Backend Service')
+order = Namespace('orders', description='Order Backend Service')
 order_db = db_client.order
 order_items_db = db_client.order_items
+menu_db = db_client.menu
 
 MODEL_order = order.model('Order',{
         'table_id' : fields.String()
@@ -26,21 +28,12 @@ MODEL_order_id = order.model('Order ID',{
 MODEL_order_item = order.model('Order Item',{
     'menu_item_id' : fields.String(),
     'amount' : fields.Float(),
-    'notes' : fields.String(),
-    'order_id' : fields.String()
+    'notes' : fields.String()
 })
-@order.route('/all')
-class OrderAll(Resource):
-    @order.doc(description='View all order')
-    def get(self):
-        orders = list(order_db.find({}))
-        for order in orders:
-            order['_id'] = str(order['_id'])
-        return orders, status.HTTP_200_OK
 
 @order.route('/<string:id>')
 class OrderManage(Resource):
-    @order.doc(description='View the Order Items in the Order, given order ID')
+    @order.doc(description='View Order, given order ID')
     def get(self, id):
         order_items = list(order_items_db.find({'order_id': id}))
         for order_item in order_items:
@@ -49,14 +42,19 @@ class OrderManage(Resource):
 
 @order.route('')
 class Order(Resource):
+    @order.doc(description='View all order')
+    def get(self):
+        orders = list(order_db.find({}))
+        for order in orders:
+            order['_id'] = str(order['_id'])
+        return orders, status.HTTP_200_OK
     @order.doc(description='Creating new Order')
     @order.expect(MODEL_order)
     def post(self):
         schema = OrderSchema()
         # table_id = request.data.get('table_id')
         order = schema.load(request.data)
-        order['status'] = 'False'
-        order['orderItems_id'] = []
+        order['status'] = 'pending'
         operation = order_db.insert_one(schema.dump(order))
         return{'inserted': str(operation.inserted_id)}, status.HTTP_201_CREATED
     @order.doc(description='Deleting an order and  the  order items in it')
@@ -65,11 +63,9 @@ class Order(Resource):
         order_id = request.data.get('id')
         # order_deleted = order_db.find({'_id':ObjectId(order_id)})
         op = order_db.delete_one({'_id':ObjectId(order_id)})
-        op2 = order_items_db.delete_many({'order_id':order_id})
-        print(op.deleted_count)
         if op.deleted_count == 0:
             return{'result' : 'No items'}, 200
-        return{'deleted':op.raw_result}, status.HTTP_204_NO_CONTENT
+        return{'status':'deleted'}, status.HTTP_204_NO_CONTENT
     @order.doc(description='Edit the status of the Order')
     @order.expect(MODEL_status)
     def put(self):
@@ -80,27 +76,44 @@ class Order(Resource):
             return{'result': 'Status Changed'}, 200
         else:
             return{'result': 'Status Invalid. Valid Status: pending, cooking, done, delivering'}, status.HTTP_400_BAD_REQUEST
-@order.route('/add')
+@order.route('/<string:order_id>')
 class OrderItem(Resource):
     @order.doc(description='Putting menu item in the order')
     @order.expect(MODEL_order_item)
-    def post(self):
+    def post(self, order_id):
         schema = OrderItemSchema()
         try:
+       
+        
             order_item = schema.load(request.data)
-            operation = order_items_db.insert_one(schema.dump(order_item))
-            order_no = request.data.get('order_id')
-            print(operation.inserted_id)
-            order_db.update_one({'_id': ObjectId(order_no)},
-                {"$push":{'orderItems_id':operation.inserted_id}})
-            return {'ordered': str(operation.inserted_id)}, status.HTTP_201_CREATED
+            menu_item_id = order_item['menu_item_id']
+
+            menu_item_queried = menu_db.aggregate([
+                {'$unwind': '$menu_items'},
+                {'$match':{"menu_items._id": menu_item_id}},
+                
+                {'$project': {'_id':"$menu_items._id",
+                            'name': '$menu_items.name',
+                            'price': '$menu_items.price'
+                }}
+        ])
+            menu_item = menu_item_queried.next()
+            
+            # print(order_item['menu_item_id'])
+            order_item['order_id'] = order_id
+            order_item['menu_item_name'] = menu_item['name']
+            order_item['menu_item_price']= menu_item['price']
+            order_db.update_one({'_id': ObjectId(order_id)},
+                {"$push":{'orderItems':schema.dump(order_item)}})
+            return order_item, status.HTTP_201_CREATED
+       
         except ValidationError as error:
             print(error)
             return{'result': 'Missing fields'}, status.HTTP_400_BAD_REQUEST
 
-@order.route('/item/<string:id>')
-class OrderItemGet(Resource):
-    @order.doc(description='Get the Order Item')
-    def get(self, id):
-        order_item = order_items_db.find_one({'_id': id})
-        return order_item, status.HTTP_200_OK
+# @order.route('/item/<string:id>')
+# class OrderItemGet(Resource):
+#     @order.doc(description='Get the Order Item')
+#     def get(self, id):
+#         order_item = order_items_db.find_one({'_id': id})
+#         return order_item, status.HTTP_200_OK
