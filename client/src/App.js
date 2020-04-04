edc85db1ddfb8daf616b3dcf84c7c932b556b3e2
Sebastian Arthur Chua <sebastian.a.chua@gmail.com>
2020-04-04 15:56:01 +1100

    fixed sessionId issues

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,13 @@ import { Register } from 'src/components/home/Register';
 import { Dashboard } from 'src/components/Dashboard';
 
 class App extends React.Component {  
-	constructor(props) {
-		super(props);
-		this.state = {
-			sessionId: ''
-		}
-	}
-
-	setSessionId = (sessionId) => {
-		this.setState({
-			sessionId: sessionId
-		})
-	}
-	
 	render() {
 		return (
 		<Router>
-			<Home path="/" setSessionId={this.setSessionId} />
-			<Login path="/login" setSessionId={this.setSessionId} />
+			<Home path="/" />
+			<Login path="/login" />
 			<Register path="/register" />      
-			<Dashboard path="/dashboard" sessionId={this.state.sessionId} setSessionId={this.setSessionId}  />
+			<Dashboard path="/dashboard" />
 		</Router>
 		)
 	}
