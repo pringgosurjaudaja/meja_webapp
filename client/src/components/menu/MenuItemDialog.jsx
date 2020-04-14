076f018392490d336efa7a53dd6d1e1f4a99319d
jmalem <z5141970@student.unsw.edu.au>
2020-04-14 20:01:15 +1000

    master
    added tabs

--- client/src/components/menu/MenuItemDialog.jsx
+++ client/src/components/menu/MenuItemDialog.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Button, Card, Alert, InputGroup, FormControl, FormLabel } from 'react-bootstrap';
+import { Modal, Button, Card, Alert, InputGroup, FormControl, FormLabel, Tab, Tabs } from 'react-bootstrap';
 import InputNumber from 'rc-input-number';
 import 'rc-input-number/assets/index.css';
 import 'src/styles/styles.css';
@@ -36,7 +36,7 @@ export class MenuItemDialog extends React.Component {
 
     render() {
         const { item, show, onHide, itemInCart } = this.props;
-
+        console.log(this.props);
         return (
             <div>
                 {item && <Modal 
@@ -47,45 +47,54 @@ export class MenuItemDialog extends React.Component {
                     <Modal.Header closeButton />
 
                     <Modal.Body>
-                        <Card.Img variant="top" src={example} />
-                        
-                        {/* Title & Description */}
-                        <Modal.Title>{item.name}</Modal.Title>
-                        <p>{item.description}</p>
-                        <p>$ {item.price}</p>
-                        
-                        {/* Notes for Menu Item */}
-                        <FormLabel>Add notes</FormLabel>
-                        <InputGroup>
-                            <FormControl 
-                                onChange={this.handleNotesChange} 
-                                as="textarea" 
-                                aria-label="With textarea" 
-                            />
-                        </InputGroup>
-                        <br></br>
-                        {/* Quantity of Menu Item */}
-                        <InputNumber 
-                            onChange={this.handleQuantityChange} 
-                            focusOplaceholder="Quantity" 
-                            min={1} 
-                            defaultValue={1} 
-                        />
-                        <br></br><br></br>
-                        <Button 
-                            onClick={this.handleAddToCart} 
-                            disabled={itemInCart(item)} 
-                            data-dismiss="modal" 
-                            variant="primary"
-                        >
-                            Add to cart
-                        </Button>
+                        <Tabs>
+                            <Tab eventKey="info" title="Info">
+                                <Card.Img variant="top" src={example} />
+                            
+                                {/* Title & Description */}
+                                <Modal.Title>{item.name}</Modal.Title>
+                                <p>{item.description}</p>
+                                <p>$ {item.price}</p>
+                                
+                                {/* Notes for Menu Item */}
+                                <FormLabel>Add notes</FormLabel>
+                                <InputGroup>
+                                    <FormControl 
+                                        onChange={this.handleNotesChange} 
+                                        as="textarea" 
+                                        aria-label="With textarea" 
+                                    />
+                                </InputGroup>
+                                <br></br>
+                                {/* Quantity of Menu Item */}
+                                <InputNumber 
+                                    onChange={this.handleQuantityChange} 
+                                    focusOplaceholder="Quantity" 
+                                    min={1} 
+                                    defaultValue={1} 
+                                />
+                                <br></br><br></br>
+                                <Button 
+                                    onClick={this.handleAddToCart} 
+                                    disabled={itemInCart(item)} 
+                                    data-dismiss="modal" 
+                                    variant="primary"
+                                >
+                                    Add to cart
+                                </Button>
 
-                        {/* Successfully Added Alert */}
-                        {itemInCart(item) &&
-                            <Alert variant="success">
-                                Added to cart.
-                            </Alert>}
+                                {/* Successfully Added Alert */}
+                                {itemInCart(item) &&
+                                    <Alert variant="success">
+                                        Added to cart.
+                                    </Alert>}
+                            </Tab>
+                            <Tab eventKey="review" title="Review">
+                                
+                            </Tab>
+                            
+                        </Tabs>
+                        
                     </Modal.Body>
                 </Modal>}
             </div>
