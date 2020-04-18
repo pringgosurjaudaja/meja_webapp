74df7db298d0f3279fe102c4f9743b132a8bd7ee
jmalem <z5141970@student.unsw.edu.au>
2020-04-19 01:56:47 +1000

    master
    wip user profile

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -5,31 +5,13 @@ import { Container, Tab, Tabs } from 'react-bootstrap';
 import { UserProfile } from 'src/components/profile/UserProfile';
 import { PastOrders } from 'src/components/profile/PastOrders';
 
-const profileTabs = {
-    PROFILE: 'profile',
-    ORDERS: 'orders',
-}
-
 export class Profile extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activeTab: profileTabs.PROFILE,
-        }
-    }
 
     render() {
         return (
             <Container>
-                <Tabs activeKey={this.state.activeTab} onSelect={(tab => this.setState({ activeTab: tab }))}>
-                    <Tab eventKey={profileTabs.PROFILE} title={"My Profile"}>
-                        <UserProfile />
-                    </Tab>
-                    <Tab eventKey={profileTabs.ORDERS} title={"Past Orders"}>
-                        <PastOrders />
-                    </Tab>
-                       
-                </Tabs>
+                <UserProfile />
+                <PastOrders />
             </Container>
         );
     }
