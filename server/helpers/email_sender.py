af13ea32a36841d63f98e648d6200a82a614c255
Rama Pringgo Surjaudaja <surjaudajapringgo@yahoo.com>
2020-04-19 00:40:55 +1000

    reservation email

--- server/helpers/email_sender.py
+++ server/helpers/email_sender.py
@@ -31,3 +31,18 @@ class EmailSender:
         with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
             server.login(self.config['sender'], password)    
             server.sendmail(self.config['sender'], recipient_email, message.as_string())
+    def send_email_text(self, recipient_email, email_content):
+        message = MIMEMultipart('alternative')
+        message['Subject'] = email_content['subject']
+        message['From'] = self.config['sender']
+        message['To'] = recipient_email
+        # The email client will try to render HTML version before plain-text
+        message.attach(MIMEText(email_content['text'], 'plain'))
+        password = 'Artemis123'     # Need to hash and store this somewhere else (SECURITY RISK)
+        port = 465
+        context = ssl.create_default_context()
+
+        # Create secure SSL Context
+        with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
+            server.login(self.config['sender'], password)    
+            server.sendmail(self.config['sender'], recipient_email, message.as_string())
