54426f31e377194dda5f08609e157439be5736c9
Elena Hadinata <elhadinata@192-168-1-100.tpgi.com.au>
2020-04-19 00:17:35 +1000

    admin css

--- admin-client/src/components/Login.jsx
+++ admin-client/src/components/Login.jsx
@@ -3,11 +3,13 @@ import {
     Button,
     Container,
     Row,
+    Col,
     Form,
 } from 'react-bootstrap';
 import { navigate } from "@reach/router";
 import 'src/styles/styles.css';
 import { Requests } from 'src/utilities/Requests';
+import logo from "src/styles/assets/logo.png";
 
 export class Login extends React.Component {
     constructor(props) {
@@ -16,6 +18,16 @@ export class Login extends React.Component {
             email: '',
             password: '',
         }
+    }
+
+    componentDidMount() {
+        let tmp = document.getElementsByTagName('body')[0];
+        tmp.setAttribute('class', 'layout--background');
+    }
+
+    componentWillUnmount() {
+        let tmp = document.getElementsByTagName('body')[0];
+        tmp.setAttribute('class', '');
     }
 
     handleChange = (event) => {
@@ -34,32 +46,42 @@ export class Login extends React.Component {
     }
 
     render() {
+        const imageAlt = "Meja Logo"
         return (
             <div className="container-home">
-                <Container>
-                    <Row>
-                        <h1 className="title">
-                            Login    
-                        </h1>
-                    </Row>
-                    <Row>
-                        <Form className="layout--padding" onSubmit={this.handleSubmit}>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Control value={this.state.email} onChange={this.handleChange}
-                                name="email" type="text" placeholder="Enter Email" />
-                            </Form.Group>
 
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Control value={this.state.password} onChange={this.handleChange}
-                                name="password" type="password" placeholder="Password" />
-                            </Form.Group>
-
-                            <Button variant="primary" onClick={this.handleSubmit}>
-                                Submit
-                            </Button>
-                        </Form>
+                <Container fluid className="l-home">
+                    <Row className="l-home__row">
+                        <Col></Col>
+                        <Col className="l-center">
+                            <img className="l-home__row__title" src={logo} alt={imageAlt}></img>
+                        </Col>
+                        <Col></Col>
                     </Row>
-                </Container>
+                    <Row className="l-home__row">
+                        <Col></Col>
+                        <Col xs={3} md={3}>
+                            <Form className="layout--padding" onSubmit={this.handleSubmit}>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Control value={this.state.email} onChange={this.handleChange}
+                                    name="email" type="text" placeholder="Enter Email" />
+                                </Form.Group>
+
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Control value={this.state.password} onChange={this.handleChange}
+                                    name="password" type="password" placeholder="Password" />
+                                </Form.Group>
+
+                                <Button className="layout--btn" variant="primary" onClick={this.handleSubmit}>
+                                    Login
+                                </Button>
+                            </Form>
+
+                        </Col>
+                        <Col></Col>
+
+                    </Row>
+            </Container>
             </div>
         );
     }
